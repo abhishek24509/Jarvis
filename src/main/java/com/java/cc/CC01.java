250a788de68f09769c07199e9cbdab7c013665d2
Abhishek Saxena <abhishek24509@gmail.com>
2017-03-29 14:39:47 +0530

    added fibonacci program based on memorization

--- src/main/java/com/java/cc/CC01.java
+++ src/main/java/com/java/cc/CC01.java
@@ -5,7 +5,10 @@ public class CC01 {
 	public static int count;
 	public static void main(String[] args) {
 		String s = "GameporterAbhishek@123";
-		premutations(s,"");
+		//premutations(s,"");
+		int n = 20;
+		int[] ar = new int[n+1];
+		System.out.println(fib(n,ar));
 	}
 
 	public static void premutations(String s , String p){
@@ -20,6 +23,20 @@ public class CC01 {
 				String rem = s.substring(0, i)+s.substring(i+1);
 				premutations(rem,p+s.charAt(i));
 			}
+		}
+	}
+	
+	public static int fib(int n,int[] ar){// O(n) is the time complexity highly efficient than O(2^n) 
+		if(n<=0){
+			return 0;
+		}else if(n==1){
+			return 1;
+		}else {
+			if(ar[n]>0){
+				return ar[n];
+			}
+			return ar[n]=fib(n-1,ar)+fib(n-2,ar);
+			
 		}
 	}
 
